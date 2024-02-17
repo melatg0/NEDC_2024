c6df709d471b09b92c917c7624935ef0a89a3826
Melat Ghebreselassie <140329398+melatg0@users.noreply.github.com>
2024-02-16 19:53:48 -0500

    updates on to dos and working organic alternative doc

---
+++ fertilizer_rec_system/main.py
@@ -0,0 +1,5 @@
+# 1 routes information from arduino to machine learning model as averages over time 
+# 2 gets reccomendation for fertilizer from classifer.pkl
+# 3 lists data from arduino, reccomendation for fertilizer, and organic/cost-effective alternatives on website
+# 4 remember to set up informative, pretty website 
+
